e8988be421d3a2df19a7c91f7b496ff574a57246
antonhopingforpeace <116687553+antonhopingforpeace@users.noreply.github.com>
2024-03-22 22:11:42 +0200

    Fix functionality of calculator. Two more problems: After = AND not displaying number when +,-,/,*

--- calculatorCode.js
+++ calculatorCode.js
@@ -1,17 +1,20 @@
 const add = function(a,b){this.textContent
-    return a+b;
+    return (a+b).toFixed(5);
 }
 
 const subtract = function(a,b){
-    return a-b;
+    return (a-b).toFixed(5);
 }
 
 const multiply = function(a,b){
-    return a*b;
+    return (a*b).toFixed(5);
 }
 
 const divide = function(a,b){
-    return a/b;
+    if(b==0){
+        return "Can't divide by zero";
+    }
+    return (a/b).toFixed(5);
 }
 
 const operate = function(num1,op,num2){
@@ -36,6 +39,7 @@ const displayScreen= document.getElementById("display");
 const hiddenScreen= document.getElementById("hidden-display");
 
 const clearKey = document.querySelector(".clear")
+const deleteKey = document.querySelector(".delete");
 
 const addKey = document.querySelector(".add");
 const subtractKey = document.querySelector(".subtract");
@@ -43,6 +47,7 @@ const multiplyKey = document.querySelector(".multiply");
 const divideKey = document.querySelector(".division");
 
 const numberKeys = document.getElementsByClassName("number");
+const decimalKey = document.querySelector(".decimal");
 
 const equalKey = document.querySelector(".result");
 
@@ -55,8 +60,24 @@ function calculateResult(){
         let numberOne = hiddenScreen.value.slice(0,hiddenScreen.value.length-1);
         let numberTwo = displayScreen.value;
         displayScreen.value = operate(numberOne,op,numberTwo);
+        while(displayScreen.value[displayScreen.value.length-1]==0){
+            displayScreen.value=displayScreen.value.slice(0,displayScreen.value.length-1);
+        }
+        if(displayScreen.value[displayScreen.value.length-1]=="."){
+            displayScreen.value=displayScreen.value.slice(0,displayScreen.value.length-1);
+        }
         hiddenScreen.value="";
+        //Here is where i will add something to protect my screen from adding extra numbers
+        // if(this.textContent=="="){
+        //     alert("fuck");
+        // }
         return displayScreen.value;
+    }
+}
+
+function deleteLastKey(){
+    if(displayScreen.value!="Can't divide by zero"){
+        displayScreen.value=displayScreen.value.slice(0,displayScreen.value.length-1);
     }
 }
 
@@ -65,7 +86,7 @@ function clearScreen(){
     hiddenScreen.value="";
 }
 
-let hiddenFlag=false;
+let flag=false;
 
 function addCalc(){
 
@@ -74,10 +95,12 @@ function addCalc(){
     
     if(val.length<15){
 
-        let hiddenFLag=false;
 
-        if(val==0 && digit!="+" && digit!="-" && digit!="*" && digit!="/"){
+        if(val==="0" && digit!="+" && digit!="-" && digit!="*" && digit!="/" && digit!="."){
             val=digit;
+        }
+        else if(val==0 && digit =="."){
+            val+=digit;
         }
         else if(val==0 && (digit=="+" || digit=="-" || digit=="*" || digit=="/")){
             return;
@@ -100,7 +123,10 @@ function addCalc(){
                 flag=false;
             }
             else{
-            val+=digit;
+                if(digit=="." && val.indexOf(".")!=-1){
+                    return;
+                }
+                val+=digit;
             }
         }
     }
@@ -110,16 +136,33 @@ function addCalc(){
 
 
 
-clearKey.addEventListener("click",clearScreen);
-
 for(let i=0;i<numberKeys.length;i++){
     numberKeys[i].addEventListener("click",addCalc);
 }
 
-const arrOfCalc = [addKey,subtractKey,multiplyKey,divideKey];
+const arrOfCalc = [addKey,subtractKey,multiplyKey,divideKey,decimalKey];
 for(let i=0;i<arrOfCalc.length;i++){
     arrOfCalc[i].addEventListener("click",addCalc);
 }
 
+function addNumber(){
+    if(hiddenScreen.value==""){
+        hiddenScreen.value=" "
+        displayScreen.value= this.textContent;
+    }
+    else{
+        displayScreen.value+=this.textContent;
+    }
+}
+
+// function refreshNumber(){
+
+// }
 
 equalKey.addEventListener("click",calculateResult);
+//equalKey.addEventListener("click",refreshNumber);
+
+clearKey.addEventListener("click",clearScreen);
+
+deleteKey.addEventListener("click",deleteLastKey);
+
